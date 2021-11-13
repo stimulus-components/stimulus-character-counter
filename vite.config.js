1b31fcb69b1d8bf0e4e2c8d111b844ff97054910
Guillaume Briday <guillaumebriday@gmail.com>
2021-11-13 20:54:16 +0100

    Upgrading to Stimulus 3 and bump dependencies

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,10 @@ module.exports = {
       name: 'stimulus-character-counter'
     },
     rollupOptions: {
-      external: ['stimulus'],
+      external: ['@hotwired/stimulus'],
       output: {
         globals: {
-          stimulus: 'Stimulus'
+          '@hotwired/stimulus': 'Stimulus'
         }
       }
     }
